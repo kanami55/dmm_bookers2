b1cd812cef3a8d48ad6c1222efccd7672b416cac
Higashimoto Kanami <summerwave5959@gmail.com>
2022-02-05 11:24:05 +0000

    [Add] アソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,22 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followerd, source: :follower
   has_one_attached :profile_image
 
+
+  validates :name, length: { in: 2..20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  
   #いいねをしているかしていないかの判定を付ける
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
   end
-
-  validates :name, length: { in: 2..20 }, uniqueness: true
-  validates :introduction, length: { maximum: 50 }
+  
 
   def get_profile_image(size)
     unless profile_image.attached?
